bdb78a7d82e4e1a995d2f41bf6db0f46713baa83
Pintu <pin2sharma907@gmail.com>
2024-11-03 21:26:21 +0530

    add data file

---
+++ data.js
@@ -0,0 +1,2 @@
+data={"Fname":"Pintu","Lname":"Sharma","Age":"26" , "Email":"pin2sharma@gmail.com"}
+module.exports=data;
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-// const http = require('http');
-// http.createServer((req, resp) => {
-//     resp.write('hello');
-//     resp.end();
-// }).listen(3000);
-console.log('hello');
+const http = require('http');
+const data = require('./data')
+http.createServer((req, resp) => {
+    resp.writeHead(200, {'Content-Type': 'application/json'}); // Corrected Content-Type
+    resp.write(JSON.stringify(data));
+    resp.end();
+}).listen(3001, () => {
+    console.log('Server is listening on http://localhost:3001/'); // Updated to 3001
+});
