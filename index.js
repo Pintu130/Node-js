48bfe0ec36eb5de35f4a4bef71d11381202a8d38
Pintu <pin2sharma907@gmail.com>
2024-11-07 18:49:39 +0530

    make get api

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-const http = require('http');
-const data = require('./data')
-http.createServer((req, resp) => {
-    resp.writeHead(200, {'Content-Type': 'application/json'}); // Corrected Content-Type
-    resp.write(JSON.stringify(data));
-    resp.end();
-}).listen(3001, () => {
-    console.log('Server is listening on http://localhost:3001/'); // Updated to 3001
+// const http = require('http');
+// const data = require('./data')
+// http.createServer((req, resp) => {
+//     resp.writeHead(200, {'Content-Type': 'application/json'}); // Corrected Content-Type
+//     resp.write(JSON.stringify(data));
+//     resp.end();
+// }).listen(3001, () => {
+//     console.log('Server is listening on http://localhost:3001/'); // Updated to 3001
+// });
+
+const express = require('express');
+const app = express();
+
+app.get('',(req,res)=>{
+    res.send('hello staring page');
 });
+
+app.get('/about',(req,res)=>{
+    res.send('hello about page');
+});
+
+app.get('/contact',(req,res)=>{
+    res.send('hello contact page');
+});
+
+app.listen(5000);
